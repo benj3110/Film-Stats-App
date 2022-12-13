9f430649c10f540daaed62c268ca0b8b692222ce
benitovark <benito.varghese@raytheon.co.uk>
2022-12-13 14:03:05 +0000

    fully changed to vite

--- Front-end/src/Components/Apis.tsx
+++ Front-end/src/Components/Apis.tsx
@@ -8,12 +8,13 @@ export async function GetFilms(props: string): Promise<FilmDataTypes[] | null> {
 
 	try {
 		const filmData: AxiosResponse<any, any> = await Axios.get(
-			`https://api.themoviedb.org/3/movie/${popular}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`
+			//todo fix the env call, not working
+			`https://api.themoviedb.org/3/movie/${popular}?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US&page=1`
 		);
 		console.log(filmData.data.results);
 		return filmData.data.results;
 	} catch (err) {
-		console.log("err");
+		console.log(err);
 		return null;
 	}
 }
